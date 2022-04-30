35d792a2b0242bcaceaf21edae25aee5bfaef291
totalsu <103229714+totalsu@users.noreply.github.com>
2022-04-30 14:44:31 +0900

    Add files via upload

---
+++ #컴퓨팅 과제 4.py
@@ -0,0 +1,13 @@
+#컴퓨팅 과제 4
+#교안 14페이지, 3개의 값
+
+print("3개의 값을 입력하시오")
+x = int(input("Enter the x:  "))
+y = int(input("Enter the y:  "))
+z = int(input("Enter the z:  "))
+
+x = x + 1
+y = y + 1
+z = z + 1
+
+print("변경된 값 : ", x, y, z)
