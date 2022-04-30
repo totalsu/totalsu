7b6ae1c9631b397f99ff818550ffc07d3f1a4c75
totalsu <103229714+totalsu@users.noreply.github.com>
2022-04-30 14:38:03 +0900

    컴퓨팅 과제 3

---
+++ #교안 5페이지, 평균구하기.py
@@ -0,0 +1,11 @@
+#교안 5페이지, 평균구하기
+
+total = 0
+counter = 1
+while counter <= 10:
+    grade = int(input ("Enter grade:  "))
+    total = grade + total
+    counter = counter + 1
+average = total / 10
+
+print(average)
