19aa5ed3e9f94be4f0d99bfdac9c5cf1da8489c7
totalsu <103229714+totalsu@users.noreply.github.com>
2022-04-30 14:52:28 +0900

    Add files via upload

---
+++ #컴퓨팅 과제 5.py
@@ -0,0 +1,14 @@
+#컴퓨팅 과제 5
+#교안 16페이지, 시급 계산
+
+a = 10
+b = 8750
+c = a * b
+
+print(c)
+
+pay_rate = 8750
+hours_worked = int(input("일을 한 전체 시간입력하세요!:  "))
+monthly_pay = pay_rate * hours_worked
+
+print(monthly_pay)
