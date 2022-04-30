a7b90aa9d1782ade20ea0c1948aab56b0f403c60
totalsu <103229714+totalsu@users.noreply.github.com>
2022-04-30 14:58:00 +0900

    Add files via upload

---
+++ #컴퓨팅 과제 6.py
@@ -0,0 +1,19 @@
+#컴퓨팅 과제 6
+
+#교안 17페이지, 환율계산 - 1
+
+from ast import ExceptHandler
+
+
+won = 100000
+usd = won * 1130
+
+print(usd)
+
+#교안 19페이지, 환율계산 - 2
+
+exchange_rate = int(input("현재 환율입력:  "))
+won = 100000
+usd = won * exchange_rate
+
+print(usd)
